7620e40db797f996b25ede76c624741e0a05b074
pzhou11 <pzhou11@users.noreply.github.com>
2017-04-09 16:10:19 -0700

    Create clean_data_lake.sh

---
+++ loading/clean_data_lake.sh
@@ -0,0 +1,48 @@
+hdfs dfs -rm /user/w205/project/arrest/clean_arrest_data.csv
+
+hdfs dfs -rm /user/w205/project/fire/clean_fire_data.csv
+
+hdfs dfs -rm /user/w205/project/bridge/clean_bridge_data.csv
+
+hdfs dfs -rm /user/w205/project/zip_reference/clean_zip_reference.csv
+
+hdfs dfs -rm /user/w205/project/death/clean_death_data.csv
+
+hdfs dfs -rm /user/w205/project/landslide/clean_landslide_data.csv
+
+hdfs dfs -rm /user/w205/project/air_quality/clean_air_quality.csv
+
+hdfs dfs -rm /user/w205/project/water_quality/clean_water_quality.csv
+
+hdfs dfs -rm /user/w205/project/water_sites/clean_water_sites.csv
+
+hdfs dfs -rm /user/w205/project/earthquake/clean_earthquake.csv
+
+hdfs dfs -rm /user/w205/project/civil_unrest/clean_civil_unrest.csv
+
+
+hdfs dfs -rmdir /user/w205/project/arrest
+
+hdfs dfs -rmdir /user/w205/project/fire
+
+hdfs dfs -rmdir /user/w205/project/bridge
+
+hdfs dfs -rmdir /user/w205/project/zip_reference
+
+hdfs dfs -rmdir /user/w205/project/death
+
+hdfs dfs -rmdir /user/w205/project/landslide
+
+hdfs dfs -rmdir /user/w205/project/air_quality
+
+hdfs dfs -rmdir /user/w205/project/water_quality
+
+hdfs dfs -rmdir /user/w205/project/water_sites
+
+hdfs dfs -rmdir /user/w205/project/earthquake
+
+hdfs dfs -rmdir /user/w205/project/civil_unrest
+
+
+hdfs dfs -rmdir /user/w205/project
+
