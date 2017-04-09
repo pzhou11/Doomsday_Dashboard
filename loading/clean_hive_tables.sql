0c2b3e58b86adbbbd36f31b684eb850d0cda87ae
pzhou11 <pzhou11@users.noreply.github.com>
2017-04-09 16:10:45 -0700

    Create clean_hive_tables.sql

---
+++ loading/clean_hive_tables.sql
@@ -0,0 +1,12 @@
+DROP TABLE air_quality;
+DROP TABLE arrest_data;
+DROP TABLE bridge_data;
+DROP TABLE civil_unrest;
+DROP TABLE death_data;
+DROP TABLE earthquake;
+DROP TABLE fire_data;
+DROP TABLE landslide_data;
+DROP TABLE water_sites;
+DROP TABLE water_quality;
+DROP TABLE zip_code_reference;
+
