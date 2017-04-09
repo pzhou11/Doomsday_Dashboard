619245c838e98dc27af34ad841dc128151e7aee3
pzhou11 <pzhou11@users.noreply.github.com>
2017-04-09 15:02:54 -0700

    Update transform_arrest.sql

--- transforming/transform_arrest.sql
+++ transforming/transform_arrest.sql
@@ -2,18 +2,20 @@ DROP TABLE T_Arrest;
 
 CREATE TABLE T_Arrest as
 
-SELECT county, 
-   sum(homicide_and_manslaughter_total) as homicide_and_manslaughter_count, 
-   sum(felony_violent_total) as felony_violent_count, 
-   sum(felony_property_total) as felony_property_count, 
-   sum(felony_drug_total) as felony_drug_count, 
-   sum(felony_sex_total) as felony_sex_count, 
-   sum(felony_other_total) as felony_other_count, 
-   sum(total_felonies) as total_felony_count,
-   sum(total_misdemeanor) as total_misdemeanor_count,
-   sum(total_status_offenses) as total_status_offense_count
+SELECT arrest_data.county, 
+   sum(arrest_data.homicide_and_manslaughter_total) as homicide_and_manslaughter_count, 
+   sum(arrest_data.felony_violent_total) as felony_violent_count, 
+   sum(arrest_data.felony_property_total) as felony_property_count, 
+   sum(arrest_data.felony_drug_total) as felony_drug_count, 
+   sum(arrest_data.felony_sex_total) as felony_sex_count, 
+   sum(arrest_data.felony_other_total) as felony_other_count, 
+   sum(arrest_data.total_felonies) as total_felony_count,
+   sum(arrest_data.total_misdemeanor) as total_misdemeanor_count,
+   sum(arrest_data.total_status_offenses) as total_status_offense_count,
+   sum(zip_code_reference.irspopulation2014) as county_population
 FROM arrest_data 
-GROUP BY county
+LEFT JOIN zip_code_reference
+   ON arrest_data.county = zip_code_reference.county
+GROUP BY arrest_data.county
 ;
-
 
